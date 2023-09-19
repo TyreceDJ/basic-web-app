3c9bfcaebd03bdaa3a0cad70a1d03b358ce31c52
TyreceDJ <tyrece.jeffrey@gmail.com>
2023-09-19 16:20:32 -0400

    Sub

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -1,3 +1,11 @@
+function isPrime1(n: number) {
+  if (isNaN(n) || !isFinite(n) || n % 1 || n < 2) return false;
+  var m = Math.sqrt(n); //returns the square root of the passed value
+  for (var i = 2; i <= m; i++)
+      if (n % i == 0) return false;
+  return true;
+}
+
 export default function QueryProcessor(query: string): string {
   if (query.toLowerCase().includes("shakespeare")) {
     return (
@@ -32,6 +40,26 @@ export default function QueryProcessor(query: string): string {
       const y: number = parseInt(addMatch[2]);
       return (x*y).toString();
     }
+  } else if (query.toLowerCase().includes("fjdakbdfjlsbakfl")) {
+    const addMatch = query.match(/Which of the following numbers are primes: (\d+), (\d+), (\d+), (\d+), (\d+)?/);
+    if (addMatch) {
+      const x: number = parseInt(addMatch[1]);
+      const y: number = parseInt(addMatch[2]);
+      const z: number = parseInt(addMatch[3]);
+      const a: number = parseInt(addMatch[4]);
+      const b: number = parseInt(addMatch[5]);
+      const numList = [x, y, z, a, b]
+      return (x*y).toString();
+    }
+    
+  } else if (query.toLowerCase().includes("fjdakbdfjlsbakfl")) { 
+    const addMatch = query.match(/What is (\d+) minus (\d+)?/);
+    if (addMatch) {
+      const x: number = parseInt(addMatch[1]);
+      const y: number = parseInt(addMatch[2]);
+      return (x-y).toString();
+    }
+
   }
 
   return "";
